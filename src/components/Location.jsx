e17c3f8b214dc428bd721c3c21ca72e3a1d816e5
Muhamad-sa <mohamedsamiir7@gmail.com>
2026-06-29 16:03:24 +0300

    feat: rename to Dar El Moshah, tap image for directions, thinner fonts

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -10,10 +10,9 @@ export default function Location({ venue }) {
         <img className="location-map" src={venue.mapImage} alt="" aria-hidden="true" loading="lazy" />
         <p className="location-name">{venue.name}</p>
         <div className="location-rule location-rule-small" aria-hidden="true" />
-        <a className="location-address" href={venue.mapUrl} target="_blank" rel="noreferrer">
-          Tap address for direction
-          <span>{venue.address}</span>
-        </a>
+        <p className="location-address">
+          {venue.address}
+        </p>
         <a className="location-photo-link" href={venue.mapUrl} target="_blank" rel="noreferrer">
           <span className="frame-corner frame-corner--tl" aria-hidden="true" />
           <span className="frame-corner frame-corner--tr" aria-hidden="true" />
